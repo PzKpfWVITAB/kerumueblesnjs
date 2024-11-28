feafdfbadb7beda5850575c9a27b2232f4344b42
PzKpfWVITAB <omneliberum01@gmail.com>
2024-11-28 08:05:57 -0600

    DB y .bat

--- install.bat
+++ install.bat
@@ -11,3 +11,9 @@ npm init --y
 npm install express mysql express-myconnection morgan ejs
 
 npm install nodemon
+
+set directorio=%CD%
+set /p contra="Introduce la contraseña de root de mysql: "
+mysql -u root -p%contra% -e "DROP DATABASE IF EXISTS kerumueblesnjs;"
+mysql -u root -p%contra% -e "CREATE DATABASE kerumueblesnjs;"
+mysql -u root -p%contra% kerumuebles_outlet < "%CD%\db\kerumueblesnjs.sql"
