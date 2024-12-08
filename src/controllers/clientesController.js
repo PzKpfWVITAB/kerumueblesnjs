ecbc2fb450855a0ba2d6eb7130e852c21523cbb3
PzKpfWVITAB <omneliberum01@gmail.com>
2024-12-08 15:15:30 -0600

    Crud funciona consulta tabla

--- src/controllers/clientesController.js
+++ src/controllers/clientesController.js
@@ -1,14 +1,24 @@
 const controller = {};
 
-controller.list = (req,res) => {
-    res.render('../views/clientes.ejs');
+controller.list = (req, res) => {
+    req.getConnection((err, conn) => {
+        if (err) {
+            return res.status(500).send(err);
+        }
+        conn.query('SELECT * FROM cliente', (err, rows) => {
+            if (err) {
+                return res.status(500).send(err);
+            }
+            res.render('../views/clientes.ejs', { data: rows });
+        });
+    });
 };
 
-controller.delete = (req,res) => {
+controller.delete = (req, res) => {
     res.send('eliminamos una cliente');
 };
 
-controller.save = (req,res) => {
+controller.save = (req, res) => {
     res.send('agregamos un cliente');
 };
 
